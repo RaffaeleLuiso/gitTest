aa0dbc593360d0fc72c4a94da3b7511cbf9a8c3c
Mario Di Biase <mariodibiase@10-26-48-65.0200.cam.it.iosda.org>
2018-03-13 12:35:56 +0100

    Ho fatto un a modifica

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Raffaele Luiso on 13/03/18.
 //  Copyright © 2018 Raffaele Luiso. All rights reserved.
-//
+// Ho fatto una modifica
 
 import UIKit
 
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label1: UILabel!
     var contatore = 0
+    var c = 0
     @IBAction func button1(_ sender: Any) {
         contatore = contatore + 1
         label1.text = "hai premuto \(contatore) volte"
