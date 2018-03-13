2e731db22755a78dfb987d387edaafee9bf0c9af
Raffaele Luiso <raffaeleluiso@10-26-48-60.0200.cam.it.iosda.org>
2018-03-13 11:42:38 +0100

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Raffaele Luiso on 13/03/18.
+//  Copyright © 2018 Raffaele Luiso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
